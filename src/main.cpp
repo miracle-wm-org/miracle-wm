251bbf4ab49d6e2566fcb659e9ad3bff265097c7
mattkae <mattkae@protonmail.com>
2023-02-18 13:10:42 -0500

    Barebones app

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <cstdlib>
 #include <cstdio>
 
-int main() {
+#include <miral/runner.h>
+#include <miral/minimal_window_manager.h>
+#include <miral/set_window_management_policy.h>
+
+int main(int argc, char const* argv[]) {
+    MirRunner runner{argc, argv};
+
+    return runner.run_with(
+        {
+            set_window_management_policy<MinimalWindowManager>()
+        });
     return EXIT_SUCCESS;
 }
