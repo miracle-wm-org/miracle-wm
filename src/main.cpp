56256bbba5f5665926e8dabb46e5e344555e6e49
mattkae <mattkae@protonmail.com>
2023-02-18 13:26:46 -0500

    Proper using statement

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <miral/minimal_window_manager.h>
 #include <miral/set_window_management_policy.h>
 
+using namespace miral;
+
 int main(int argc, char const* argv[]) {
     MirRunner runner{argc, argv};
 
