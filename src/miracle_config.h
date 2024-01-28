1cb06fa7df7ad2dd7410214714c91dcd7d68a6f6
Matthew Kosarek <matt.kosarek@canonical.com>
2024-01-28 10:52:28 -0500

    feature: providing keybindings via a configuration; next step is loading it

---
+++ src/miracle_config.h
@@ -0,0 +1,155 @@
+#ifndef MIRACLEWM_MIRACLE_CONFIG_H
+#define MIRACLEWM_MIRACLE_CONFIG_H
+
+#include <string>
+#include <miral/toolkit_event.h>
+#include <vector>
+#include <linux/input.h>
+
+namespace miracle
+{
+
+enum DefaultKeyCommand
+{
+    Terminal = 0,
+    RequestVertical,
+    RequestHorizontal,
+    ToggleResize,
+    MoveUp,
+    MoveDown,
+    MoveLeft,
+    MoveRight,
+    SelectUp,
+    SelectDown,
+    SelectLeft,
+    SelectRight,
+    QuitActiveWindow,
+    QuitCompositor,
+    MAX
+};
+
+
+struct KeyCommand
+{
+    MirKeyboardAction action;
+    uint modifiers;
+    int key;
+};
+
+typedef std::vector<KeyCommand> KeyCommandList;
+
+class MiracleConfig
+{
+public:
+    MiracleConfig();
+    [[nodiscard]] MirInputEventModifier get_input_event_modifier() const;
+    [[nodiscard]] DefaultKeyCommand matches_key_command(MirKeyboardAction action, int scan_code, unsigned int modifiers) const;
+
+private:
+    static const int  miracle_input_event_modifier_default = 1 << 18;
+    MirInputEventModifier modifier = mir_input_event_modifier_meta;
+    KeyCommandList key_commands[DefaultKeyCommand::MAX] = {
+        {
+            {
+                MirKeyboardAction::mir_keyboard_action_down,
+                miracle_input_event_modifier_default,
+                KEY_ENTER
+            }
+        },
+        {
+            {
+                MirKeyboardAction::mir_keyboard_action_down,
+                miracle_input_event_modifier_default,
+                KEY_V
+            }
+        },
+        {
+            {
+                MirKeyboardAction::mir_keyboard_action_down,
+                miracle_input_event_modifier_default,
+                KEY_H
+            }
+        },
+        {
+            {
+                MirKeyboardAction::mir_keyboard_action_down,
+                miracle_input_event_modifier_default,
+                KEY_R
+            }
+        },
+        {
+            {
+                MirKeyboardAction::mir_keyboard_action_down,
+                miracle_input_event_modifier_default | mir_input_event_modifier_shift,
+                KEY_UP
+            }
+        },
+        {
+            {
+                MirKeyboardAction::mir_keyboard_action_down,
+                miracle_input_event_modifier_default | mir_input_event_modifier_shift,
+                KEY_DOWN
+            }
+        },
+        {
+            {
+                MirKeyboardAction::mir_keyboard_action_down,
+                miracle_input_event_modifier_default | mir_input_event_modifier_shift,
+                KEY_LEFT
+            }
+        },
+        {
+            {
+                MirKeyboardAction::mir_keyboard_action_down,
+                miracle_input_event_modifier_default | mir_input_event_modifier_shift,
+                KEY_RIGHT
+            }
+        },
+        {
+            {
+                MirKeyboardAction::mir_keyboard_action_down,
+                miracle_input_event_modifier_default,
+                    KEY_UP
+            }
+        },
+        {
+            {
+                MirKeyboardAction::mir_keyboard_action_down,
+                miracle_input_event_modifier_default,
+                    KEY_DOWN
+            }
+        },
+        {
+            {
+                MirKeyboardAction::mir_keyboard_action_down,
+                miracle_input_event_modifier_default,
+                    KEY_LEFT
+            }
+        },
+        {
+            {
+                MirKeyboardAction::mir_keyboard_action_down,
+                miracle_input_event_modifier_default,
+                    KEY_RIGHT
+            }
+        },
+        {
+            {
+                MirKeyboardAction::mir_keyboard_action_down,
+                miracle_input_event_modifier_default | mir_input_event_modifier_shift,
+                KEY_Q
+            }
+        },
+        {
+            {
+                MirKeyboardAction::mir_keyboard_action_down,
+                miracle_input_event_modifier_default | mir_input_event_modifier_shift,
+                KEY_E
+            }
+        },
+    };
+};
+}
+
+
+#endif
