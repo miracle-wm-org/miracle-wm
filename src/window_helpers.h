d626ce67d3b3692c39aebbc3b722ffea70499e9a
Matthew Kosarek <matthew@matthewkosarek.xyz>
2024-06-23 10:06:29 -0400

    (#178) bugfix: fix for menus no longer being selectable (#179)

--- src/window_helpers.h
+++ src/window_helpers.h
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef MIRACLEWM_WINDOW_HELPERS_H
 #define MIRACLEWM_WINDOW_HELPERS_H
 
+#include "window_metadata.h"
 #include <miral/window_info.h>
 #include <miral/window_manager_tools.h>
 
@@ -25,23 +26,25 @@ namespace miracle
 {
 class LeafNode;
 class TilingWindowTree;
-class WindowMetadata;
 
 namespace window_helpers
 {
     bool is_window_fullscreen(MirWindowState state);
 
     template <typename T>
-    bool is_tileable(T const& requested_specification)
+    WindowType get_ideal_type(T const& requested_specification)
     {
-        auto t = requested_specification.type();
-        auto state = requested_specification.state();
         auto has_exclusive_rect = requested_specification.exclusive_rect().is_set();
-        auto has_parent = requested_specification.parent().is_set();
-        return (t == mir_window_type_normal || t == mir_window_type_freestyle)
-            && (state == mir_window_state_restored || state == mir_window_state_maximized)
-            && !has_exclusive_rect
-            && !has_parent;
+        if (has_exclusive_rect)
+            return WindowType::other;
+
+        auto t = requested_specification.type();
+        if (t == mir_window_type_normal || t == mir_window_type_freestyle)
+        {
+            return WindowType::none;
+        }
+
+        return WindowType::other;
     }
 
     std::shared_ptr<WindowMetadata> get_metadata(
