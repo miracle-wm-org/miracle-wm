c07a62aed330b6d67a20cecf212723ef62912306
Matthew Kosarek <matthew@matthewkosarek.xyz>
2024-07-14 16:35:55 -0400

    backout: renderer.cpp changes

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -87,11 +87,6 @@ public:
         return get_rectangle(clip_area_rect.value());
     }
 
-    geom::RectangleD src_bounds() const override
-    {
-        return {};
-    }
-
     [[nodiscard]] float alpha() const override
     {
         return _alpha;
