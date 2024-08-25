26c128d66c564a26f9a849b11e18c38b832c71ab
Matthew Kosarek <matt.kosarek@canonical.com>
2024-08-25 08:19:47 -0400

    refactor: move the server to its own file in tests to be shared

---
+++ tests/ipc/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from subprocess import Popen, PIPE, STDOUT
+import os
+
+@pytest.fixture()
+def server():
+    if "MIRACLE_IPC_TEST_USE_ENV" in os.environ:
+        yield os.environ["SWAYSOCK"]
+        return
+    
+    command = "miracle-wm"
+    if "MIRACLE_IPC_TEST_BIN" in os.environ:
+        command = os.environ["MIRACLE_IPC_TEST_BIN"]
+    
+    env = os.environ.copy()
+    env['WAYLAND_DISPLAY'] = 'wayland-98'
+    process = Popen([command, '--platform-display-libs', 'mir:virtual', '--virtual-output', '800x600'],
+                    env=env, stdout=PIPE, stderr=STDOUT)
+    
+    socket = ""
+    to_find = "Listening to IPC socket on path: "
+    with process.stdout:
+        for line in iter(process.stdout.readline, b''):
+            data = line.decode("utf-8").strip()
+            if to_find in data:
+                i = data.index(to_find)
+                i = i + len(to_find)
+                socket = data[i:].strip()
+                break
+
+        yield socket
+        process.terminate()
+        return
