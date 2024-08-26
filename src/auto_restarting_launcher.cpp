45b54b03dacda902faef2b3849c83d84815e1794
Matthew Kosarek <matt.kosarek@canonical.com>
2024-08-26 19:32:15 -0400

    meowgit add src/auto_restarting_launcher.cpp !

--- src/auto_restarting_launcher.cpp
+++ src/auto_restarting_launcher.cpp
@@ -44,7 +44,8 @@ void AutoRestartingLauncher::launch(miracle::StartupApp const& cmd)
     }
     mir::log_info("Started external client %s with pid=%d", cmd.command.c_str(), pid);
 
-//    pid_to_command_map[pid] = cmd;
+    if (cmd.restart_on_death || cmd.should_halt_compositor_on_death)
+        pid_to_command_map[pid] = cmd;
 }
 
 void AutoRestartingLauncher::kill_all()
