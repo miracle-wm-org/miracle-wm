4d37bf9ae4e816383d7ded0cb0762faf17c0f66e
Matthew Kosarek <matt.kosarek@canonical.com>
2025-03-26 19:28:04 -0400

    Chaning name

--- tools/clang-tidy.sh
+++ tools/clang-tidy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 find ./src/ -regex '.*\.\(cpp\|h\)' -exec /usr/bin/clang-format -style=file -i {} \;
-find ./libmiracle-wm-config/src/ -regex '.*\.\(cpp\|h\)' -exec /usr/bin/clang-format -style=file -i {} \;
-find ./libmiracle-wm-config/include/ -regex '.*\.\(cpp\|h\)' -exec /usr/bin/clang-format -style=file -i {} \;
+find ./miracle-wm-config/src/ -regex '.*\.\(cpp\|h\)' -exec /usr/bin/clang-format -style=file -i {} \;
+find ./miracle-wm-config/include/ -regex '.*\.\(cpp\|h\)' -exec /usr/bin/clang-format -style=file -i {} \;
 find ./tests/ -regex '.*\.\(cpp\|h\)' -exec /usr/bin/clang-format -style=file -i {} \;
 find ./miraclemsg/ -regex '.*\.\(cpp\|h\)' -exec /usr/bin/clang-format -style=file -i {} \;
