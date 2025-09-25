e4eb706fc68fbd05bf192ddbe5141a4dec3e70fb
Matthew Kosarek <matthew@matthewkosarek.xyz>
2025-09-24 20:37:55 -0400

    task: when a window is floated, it is centered on its output (#668)

--- src/math_helpers.h
+++ src/math_helpers.h
@@ -18,27 +18,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef MIRACLE_WM_MATH_HELPERS_H
 #define MIRACLE_WM_MATH_HELPERS_H
 
-#include <cmath>
-#include <mir/geometry/point.h>
-#include <mir/geometry/rectangle.h>
-
-namespace geom = mir::geometry;
-
 namespace miracle
 {
-geom::Point get_center(geom::Rectangle const& r)
+template <typename T>
+float as_float(T const& value)
 {
-    return {
-        r.top_left.x.as_int() + static_cast<int>(((float)r.size.width.as_int()) / 2.f),
-        r.top_left.x.as_int() + static_cast<int>(((float)r.size.height.as_int()) / 2.f)
-    };
-}
-
-float get_distance(geom::Point const& p1, geom::Point const& p2)
-{
-    auto const y_diff = p2.x.as_int() - p1.x.as_int();
-    auto const x_diff = p2.y.as_int() - p1.y.as_int();
-    return sqrtf(x_diff * x_diff + y_diff * y_diff);
+    return static_cast<float>(value.as_int());
 }
 }
 
