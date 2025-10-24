327063980203b92beedbb7b623602306a7fed243
Matthew Kosarek <matthew@matthewkosarek.xyz>
2025-10-24 15:23:22 -0700

    task: add opaque region to surfaces and renderables + add -DCOMPILING_AGAINST_DEV so that CI works (#682)
    * task: add opaque region to surfaces and renderables
    
    * Add the compiling against dev flag

--- src/mir_version_manager.h
+++ src/mir_version_manager.h
@@ -40,4 +40,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MIR_VERSION_2_22_OR_GREATER
 #endif
 
+#if ((MIR_SERVER_MAJOR_VERSION > 2) || (MIR_SERVER_MAJOR_VERSION == 2 && MIR_SERVER_MINOR_VERSION >= 24)) || COMPILING_AGAINST_DEV
+#define MIR_VERSION_2_24_OR_GREATER
+#endif
+
 #endif // MIRACLE_MIR_VERSION_MANAGER_H
