3161b74ee73d7e6249871fb2e7ff4ee7717a4416
Matt Kosarek <matt.kosarek@canonical.com>
2025-11-17 08:28:41 -0500

    task: bump version to 0.8.2

--- src/version.h
+++ src/version.h
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define MIRACLE_WM_MAJOR 0
 #define MIRACLE_WM_MINOR 8
-#define MIRACLE_WM_PATCH 0
+#define MIRACLE_WM_PATCH 2
 
-#define MIRACLE_VERSION_STRING "0.8.0"
+#define MIRACLE_VERSION_STRING "0.8.2"
 
 #endif // MIRACLE_WM_VERSION_H
