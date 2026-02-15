6f0a77091c189b03c95bfa7ffb534477e9652d99
Matt Kosarek <matt.kosarek@canonical.com>
2026-02-15 11:21:06 -0500

    Adding other animation functions

--- miracle-plugin-rs/src/plugin.rs
+++ miracle-plugin-rs/src/plugin.rs
@@ -7,6 +7,30 @@ pub trait Plugin {
     fn window_open_animation(&self, data: &AnimationFrameData) -> Option<AnimationFrameResult> {
         None
     }
+
+    /// Handles the window closing animation.
+    ///
+    /// If None is returned, the animation is not handled by this plugin.
+    fn window_close_animation(&self, data: &AnimationFrameData) -> Option<AnimationFrameResult> {
+        None
+    }
+
+    /// Handles the window movement animation.
+    ///
+    /// If None is returned, the animation is not handled by this plugin.
+    fn window_move_animation(&self, data: &AnimationFrameData) -> Option<AnimationFrameResult> {
+        None
+    }
+
+    /// Handles the workspace switching animation.
+    ///
+    /// If None is returned, the animation is not handled by this plugin.
+    fn workspace_switch_animation(
+        &self,
+        data: &AnimationFrameData,
+    ) -> Option<AnimationFrameResult> {
+        None
+    }
 }
 
 #[macro_export]
@@ -46,7 +70,52 @@ macro_rules! miracle_plugin {
                                     as *mut $crate::bindings::miracle_plugin_animation_frame_result_t);
                                 *out = c_result;
                             }
-                            1
+                            return 1;
+                        }
+                        None => 0,
+                    }
+                },
+                $crate::bindings::miracle_animation_type_miracle_animation_type_window_close => {
+                    match plugin.window_close_animation(&data) {
+                        Some(result) => {
+                            let c_result: $crate::bindings::miracle_plugin_animation_frame_result_t =
+                                result.into();
+                            unsafe {
+                                let out = &mut *(result_ptr
+                                    as *mut $crate::bindings::miracle_plugin_animation_frame_result_t);
+                                *out = c_result;
+                            }
+                            return 1;
+                        }
+                        None => 0,
+                    }
+                },
+                $crate::bindings::miracle_animation_type_miracle_animation_type_window_move => {
+                    match plugin.window_move_animation(&data) {
+                        Some(result) => {
+                            let c_result: $crate::bindings::miracle_plugin_animation_frame_result_t =
+                                result.into();
+                            unsafe {
+                                let out = &mut *(result_ptr
+                                    as *mut $crate::bindings::miracle_plugin_animation_frame_result_t);
+                                *out = c_result;
+                            }
+                            return 1;
+                        }
+                        None => 0,
+                    }
+                },
+                $crate::bindings::miracle_animation_type_miracle_animation_type_workspace_switch => {
+                    match plugin.workspace_switch_animation(&data) {
+                        Some(result) => {
+                            let c_result: $crate::bindings::miracle_plugin_animation_frame_result_t =
+                                result.into();
+                            unsafe {
+                                let out = &mut *(result_ptr
+                                    as *mut $crate::bindings::miracle_plugin_animation_frame_result_t);
+                                *out = c_result;
+                            }
+                            return 1;
                         }
                         None => 0,
                     }
