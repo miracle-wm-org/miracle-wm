8310b2ca721a7bab3fb290596756f6432fa6e75d
Matthew Kosarek <matthew@matthewkosarek.xyz>
2026-05-03 15:20:25 -0400

    feature: add possibility for custom sampler functions in window rendering shaders (#872)

--- src/program_factory.cpp
+++ src/program_factory.cpp
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
 
+#include <cstdint>
 #define MIR_LOG_COMPONENT "program_factory"
 
 #include "program_factory.h"
@@ -263,6 +264,31 @@ void main() {
 
     // We delete the shaders here. This is fine; it only marks them
     // for deletion. GL will only delete them once the GL Program they're linked in is destroyed.
+}
+
+mir::graphics::gl::Program& miracle::ProgramFactory::resolve_custom(uint8_t id)
+{
+    std::lock_guard lock { custom_mutex };
+    for (auto const& custom : custom_samplers)
+    {
+        if (custom.id == id)
+        {
+            return compile_fragment_shader(
+                reinterpret_cast<void*>(id),
+                "",
+                custom.sample_to_rgba_func.c_str());
+        }
+    }
+
+    throw std::runtime_error("Unknown custom sampler id");
+}
+
+uint8_t miracle::ProgramFactory::register_sample_to_rgba(std::string sample_to_rgba_func)
+{
+    std::lock_guard lock { custom_mutex };
+    auto const next_id = id++;
+    custom_samplers.push_back(CustomSamplers { next_id, std::move(sample_to_rgba_func) });
+    return next_id;
 }
 
 GLuint miracle::ProgramFactory::compile_shader(GLenum type, GLchar const* src)
