a030218d3b9e84fa1865c53828de65d9ad48e31f
Matt Kosarek <matt.kosarek@canonical.com>
2026-06-23 08:54:44 -0400

    fix tests

--- tests/test_workspace.cpp
+++ tests/test_workspace.cpp
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
 
+#define GLM_ENABLE_EXPERIMENTAL
 #include "compositor_state.h"
 #include "leaf_container.h"
 #include "mock_container.h"
