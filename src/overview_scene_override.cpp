fd09307585925a5483602d45396c2605da1138f4
Matt Kosarek <matt.kosarek@canonical.com>
2026-08-30 13:04:18 -0400

    Minor comment cleanup

--- src/overview_scene_override.cpp
+++ src/overview_scene_override.cpp
@@ -20,7 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "animator.h"
 #include "compositor_state.h"
 #include "config.h"
-#include "output.h"
 #include "output_manager.h"
 #include "overview_scene_override_delegate.h"
 #include "shell_component_container.h"
@@ -30,7 +29,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "workspace_preview.h"
 
 #include <algorithm>
-#include <cmath>
 #include <glm/gtc/matrix_transform.hpp>
 #include <mir/scene/surface.h>
 #include <miral/toolkit_event.h>
